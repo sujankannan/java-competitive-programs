1427a1c7d4e9e8641bdff0f1ccad4fa0e713253b
Sujan <sujankannan08@gmail.com>
2023-07-27 16:39:35 +0530

    Add files via upload

---
+++ RightTrainglePattern.java
@@ -0,0 +1,17 @@
+ 	public class RightTrainglePattern {
+
+    public static void main(String[] args) {	
+	
+	int i, j;    				//right triangle star pattern
+      
+	  for (i = 1; i <= 6; i++) {
+
+            for (j = 1; j < i; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
