e78f05efcb4ad268149d2705f577c4ac8559d3e8
Sujan <sujankannan08@gmail.com>
2023-10-02 16:23:47 +0530

    Add files via upload
    square Star Pattern using java

---
+++ squareStarPattern.java
@@ -0,0 +1,20 @@
+//square Star Pattern
+
+public class squareStarPattern {
+
+
+    public static void main(String[] args) {
+
+        int n=5,i,j;
+
+        for (i=0;i<n;i++){
+
+            for (j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
